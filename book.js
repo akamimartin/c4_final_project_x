34fc5f3596a845c1a4a208649c3da26b93ccc72a
Janet <akamimartin@gmail.com>
2015-06-16 16:45:54 -0700

    JM latest changes

--- book.js
+++ book.js
@@ -1,3 +1,38 @@
-$(document).ready(function() {
-     
+
+function book1() {
+	console.log("in the boo1 function");
 }
+
+
+
+
+
+
+
+$(document).ready(function() {
+    console.log('page is loading');
+
+    	var paragraphs = $('.content').find('p');
+
+    	$.each(paragraphs, function(){
+    		var p = $(this);
+    		var text = p.text().trim();
+
+    		console.log(text);
+    	
+	    	var newText = text.replace(/([^ |^\n]+)([ |\n|]+|$)/gi, " $1");
+	    	var textArr = newText.split(" ");
+
+	    	console.log("textArr : ", textArr);
+
+	    	var newTextElements = '<span class="txt-con"> '+textArr.join('</span><span class="txt-con"> ')+'</span>';
+
+	    	p.html(newTextElements);
+    	});
+
+    	$('body').on('click', '.txt-con', function(){
+    		var elm = $(this);
+    		var value = elm.html();
+    		console.log("you just clicked on : ", value);
+    	});   
+});
